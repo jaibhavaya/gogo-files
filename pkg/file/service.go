a8b201e3357c3a0f826035851027c468d141b8a8
Jacob Lawson <jacob@lawmatics.com>
2025-04-13 19:59:36 -0700

    start adding method for syncing file

--- pkg/file/service.go
+++ pkg/file/service.go
@@ -8,7 +8,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 )
 
-func (f *Service) GetObject(bucket, key string) (*s3.GetObjectOutput, error) {
+const FOUR_MB int64 = 4 * 1024 * 1024
+
+func (f *Service) getObject(bucket, key string) (*s3.GetObjectOutput, error) {
 	result, err := f.s3Client.GetObject(context.TODO(), &s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
@@ -18,4 +20,23 @@ func (f *Service) GetObject(bucket, key string) (*s3.GetObjectOutput, error) {
 	}
 
 	return result, nil
+}
+
+func (f *Service) SyncFile(bucket, key string) error {
+	fileReader, err := f.getObject(bucket, key)
+	if err != nil {
+		return fmt.Errorf("failed to get file reader: %v", err)
+	}
+
+	defer fileReader.Body.Close()
+
+	size := *fileReader.ContentLength
+
+	if size < FOUR_MB {
+		fmt.Println("Under four mb! sync normally")
+	} else {
+		fmt.Println("Over 4mb! stream sync this bad boy!")
+	}
+
+	return nil
 }
